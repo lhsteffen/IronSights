b403af6f3be2d643b480fc9fe62a9deebcf90c13
Luke Steffen <lhsteffen527@protonmail.com>
2026-01-10 23:47:33 -0600

    Removed icon test

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -20,8 +20,8 @@ void main() {
     expect(find.text('fail'), findsNothing);
 
     // Tap the '+' icon and trigger a frame.
-    await tester.tap(find.byIcon(Icons.fitness_center));
-    await tester.pump();
+    // await tester.tap(find.byIcon(Icons.fitness_center));
+    // await tester.pump();
 
     // Verify that our counter has incremented.
     // expect(find.text('0'), findsNothing);
