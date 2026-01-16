d0a87ca5c446fe45db18d5513f8cfe859a9b7d37
Luke Steffen <lhsteffen527@protonmail.com>
2026-01-16 00:23:14 -0600

    Added toggle button for work or light/heavy to new lift page. Added work weight entry

--- lib/new_lift.dart
+++ lib/new_lift.dart
@@ -12,6 +12,8 @@ import 'package:iron_sights/main.dart';
 import 'package:provider/provider.dart';
 import 'lift.dart';
 
+const List<Widget> weightTypes = <Widget>[Padding(padding: EdgeInsets.only(left: 10, right: 10), child: Text("Work")), Padding(padding: EdgeInsets.only(left: 10, right: 10), child: Text("Light/Heavy"))];
+
 class NewLift extends StatefulWidget {
   const NewLift({super.key});
 
@@ -21,6 +23,9 @@ class NewLift extends StatefulWidget {
 
 class _NewLiftState extends State<NewLift> {
   Lift newLift = Lift.emptyLift();
+
+  final List<bool> _selectedWeightType = <bool>[true, false];
+  bool vertical = false;
 
   @override
   Widget build(BuildContext context) {
@@ -54,8 +59,36 @@ class _NewLiftState extends State<NewLift> {
             const SizedBox(
               height: 10.0,
             ),
+            ToggleButtons(
+              direction: vertical ? Axis.vertical : Axis.horizontal,
+              borderRadius: const BorderRadius.all(Radius.circular(8)),
+              constraints: const BoxConstraints(minHeight: 40.0, minWidth: 80.0),
+              isSelected: _selectedWeightType,
+              onPressed: (int index) {
+                setState(() {
+                  for (int i = 0; i < _selectedWeightType.length; i++) {
+                    _selectedWeightType[i] = i == index;
+                  }
+                });
+              },
+              children: weightTypes,
+            ),
             Text(
-              ""
+              "Work Weight",
+              style: TextStyle(
+                fontSize: 14,
+                color: Colors.grey,
+                fontWeight: FontWeight.w500
+              ),
+            ),
+            Flexible(
+              child: TextField(
+                decoration: InputDecoration(
+                  border: OutlineInputBorder(
+                    borderRadius: BorderRadius.all(Radius.circular(10.0))
+                  )
+                ),
+              ),
             ),
             Flexible(
               fit: FlexFit.loose,
@@ -66,7 +99,7 @@ class _NewLiftState extends State<NewLift> {
                     child: Column(
                       children: [
                         Text(
-                          "Heavy Weight",
+                          "Light Weight",
                           style: TextStyle(
                             fontSize: 14,
                             color: Colors.grey,
@@ -85,13 +118,13 @@ class _NewLiftState extends State<NewLift> {
                       ],
                     ),
                   ),
-                  const SizedBox(width: 5.0,),
+                  const SizedBox(width: 5.0),
                   Flexible(
                     fit: FlexFit.loose,
                     child: Column(
                       children: [
                         Text(
-                          "Light Weight",
+                          "Heavy Weight",
                           style: TextStyle(
                             fontSize: 14,
                             color: Colors.grey,
